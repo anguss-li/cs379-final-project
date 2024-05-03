7c42031af1540fa95ce432a557f74bedcf94e739
Angus Li <angusr.li02@gmail.com>
2024-05-03 19:04:57 -0400

    Improvements to DAG learning

--- pytetrad/PROJECT_create_dag.py
+++ pytetrad/PROJECT_create_dag.py
@@ -16,10 +16,10 @@ df = df[
     [
         "T",
         "RepDrt",
-        "loc_rules_bi",
-        "trad_rules_bi",
-        "more_loc_rules_bi",
-        "more_trad_rules_bi",
+        "loc_rules_bi", # the presence of local official government rules regulating natural resources;
+        "trad_rules_bi", # the presence of informal nongovernmental traditional rules regulating natural resources;
+        "more_loc_rules_bi", # a greater number of local official government rules regulating natural resources than there were in the past;
+        "more_trad_rules_bi", # a greater number of informal nongovernmental traditional rules regulating natural resources than there were in the past.
         "TAMBelow1st",
         "TAMBelow2nd",
         "SPI3Below1st",
@@ -27,16 +27,16 @@ df = df[
         "vio_support",
         "DataVCI",
         "DataTI3",
-        "SumPre5_50k",
-        "SPI3mean2013_07_2014_06",
-        "Var_low",
+        "SumPre5_50k", # Violence within fifty kilometers (five year)
+        "SPI3mean2013_07_2014_06", # Precipitation deviation (enumeration year)
+        "Var_low", # Precipitation variation low
         "age",
         "gender",
         "employed_q3",
         "formal_education_q5",
         "pastoral_q6",
-        "low_ses_q15",
-        "included_q11",
+        "low_ses_q15", # Low socioeconomic status
+        "included_q11", # Included previous government
         "attacked_1yr",
         "eth_match",
         "gender_match",
@@ -62,12 +62,24 @@ df.to_csv("PROJECT_data.csv", index=False, na_rep="*")
 
 search = ts.TetradSearch(df)
 
-# TODO: Conditional Correlation Independence
 ## Use appropriate test of independence
-search.use_sem_bic(penalty_discount=1)
+search.use_conditional_gaussian_score(penalty_discount=1)
 
 # Set knowledge
-# search.add_to_tier(0, "")
+search.add_to_tier(0, "DataVCI")
+search.add_to_tier(0, "DataTI3")
+search.add_to_tier(0, "SumPre5_50k")
+search.add_to_tier(0, "SPI3mean2013_07_2014_06")
+search.add_to_tier(0, "Var_low")
+search.add_to_tier(0, "age")
+search.add_to_tier(0, "gender")
+search.add_to_tier(1, "low_ses_q15")
+search.add_to_tier(1, "pastoral_q6")
+search.add_to_tier(1, "employed_q3")
+search.add_to_tier(1, "formal_education_q5")
+search.add_to_tier(1, "pastoral_q6")
+search.add_to_tier(1, "gender_match")
+search.add_to_tier(1, "vio_support")
 
 ## Run the search
 
